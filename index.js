93288662d54d983e47d60129dba1416a84ef0527
Catalin Rosu <me@catalin.red>
2018-06-25 19:55:09 +0300

    Serve static files in Express.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'pug');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.render('index')
 })
